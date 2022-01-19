5615ac2ac8fed79ffbccf6c7e8a9cd30c05ecf9f
Mzcoder-hub <masterbama99@gmail.com>
2022-01-19 20:54:29 +0700

    add Exception Library in TransactionController

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -7,6 +7,7 @@ use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use App\Models\TransactionItem;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -47,37 +48,37 @@ class TransactionController extends Controller
 
     public function checkout(Request $request)
     {
-	try{
-        $request->validate([
-            'items' => 'required|array',
-            'seat_number' => 'required',
-	    'items.*.id' => 'exists:menus,id',
-            'total_price' => 'required',
-            'status' => 'required',
-        ]);
-//        return ResponseFormatter::success($request->items,'Transaksi Berhasil');
-        $transaction = Transaction::create([
-            'users_id' => Auth::user()->id,
-            'seat_number' => $request->seat_number,
-            'total_price' => $request->total_price,
-            'status' => $request->status
-        ]);
-
-//        return ResponseFormatter::success($transaction->id, 'Transaction Success');
-
-        foreach ($request->items as $menu) {
-            TransactionItem::create([
-               'menus_id' => $menu['id'],
-               'transactions_id' => $transaction->id,
-               'quantity' => $menu['quantity']
+        try {
+            $request->validate([
+                'items' => 'required|array',
+                'seat_number' => 'required',
+                'items.*.id' => 'exists:menus,id',
+                'total_price' => 'required',
+                'status' => 'required',
             ]);
-        }
+            //        return ResponseFormatter::success($request->items,'Transaksi Berhasil');
+            $transaction = Transaction::create([
+                'users_id' => Auth::user()->id,
+                'seat_number' => $request->seat_number,
+                'total_price' => $request->total_price,
+                'status' => $request->status
+            ]);
 
-	DB::commit();
-        return ResponseFormatter::success($transaction, 'Transaksi berhasil');
-	}catch(Exception $e){
-        DB::rollback();
-        return ResponseFormatter::error($e, 'Transaksi Gagal');
+            //        return ResponseFormatter::success($transaction->id, 'Transaction Success');
+
+            foreach ($request->items as $menu) {
+                TransactionItem::create([
+                    'menus_id' => $menu['id'],
+                    'transactions_id' => $transaction->id,
+                    'quantity' => $menu['quantity']
+                ]);
+            }
+
+            DB::commit();
+            return ResponseFormatter::success($transaction, 'Transaksi berhasil');
+        } catch (Exception $e) {
+            DB::rollback();
+            return ResponseFormatter::error($e, 'Transaksi Gagal');
         }
     }
 }
